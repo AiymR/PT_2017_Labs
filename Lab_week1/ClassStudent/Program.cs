daaed59d507e4b89c030864c7c9864316f64059f
Aiym <raikhanovaaiym@gmail.com>
2017-02-04 23:43:56 +0600

    first commit

---
+++ Lab_week1/ClassStudent/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassStudent
+{
+    class Student
+
+    {
+        public string name;
+        public string surname;
+        public double gpa;
+
+        public Student()
+        {
+            name = "John";
+            surname = "Doe";
+            gpa = 3.5;
+
+        }
+        public Student(string name, string surname, int gpa)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.gpa = gpa;
+
+        }
+        public override string ToString()
+        {
+            return name + " " + surname + " " + gpa;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("John", "Doe", 4);
+            Console.WriteLine(s.ToString());
+            Console.ReadKey();
+        }
+    }  
+}
+
+
---
+++ Lab_week1/Lab_week1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_week1
+{
+    class Program
+    {
+        static bool IsPrime(string x) {
+            int a = int.Parse(x);
+
+            for (int i = 2; i <= Math.Sqrt(a); ++i)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            if (a < 2) return false;
+
+
+            return true;
+        }
+    
+static void Main(string[] args)
+{
+
+    for (int i = 0; i < args.Length; ++i)
+    {
+        if (IsPrime(args[i])) //
+        {
+            Console.WriteLine(args[i]); //
+        }
+    }
+    Console.ReadKey();
+ }
+    }
+}
